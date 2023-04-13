2e60ab1828c261a3ef3a7dc1958dbd248d5a7e8f
astoppop <michealhuang7@gmail.com>
2023-04-13 00:19:13 -0400

    kitchen updates

--- js/scrolling_kitchen.js
+++ js/scrolling_kitchen.js
@@ -9,12 +9,11 @@ const camera = new PerspectiveCamera(45, $(window).outerWidth() / $(window).heig
 camera.position.set(10, 20, 10);
 
 const renderer = new WebGLRenderer({ antialias: true, alpha: true });
-// renderer.physicallyCorrectLights = true;
 renderer.outputEncoding = sRGBEncoding;
 renderer.toneMapping = LinearToneMapping;
 renderer.setSize($(window).outerWidth(), $(window).height());
 
-const controls = new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
 
 $(window).resize(() => {
     camera.aspect = $(window).outerWidth() / $(window).height();
@@ -35,26 +34,64 @@ loader.load('../assets/kitchen.glb', (glb) => {
     scene.add(glb.scene);
 });
 
-const cameraPositions = [
-    [[10, 20, 10], [10, 10, 10], 10],
-    [[10, 10, 10], [10, 20, 10], 30],
-];
-let totalPositionWeight = 0;
-for (let position of cameraPositions) { totalPositionWeight += position[2]; }
+// convert from
+// weight, from, to
+// to
+// index, from, to
+const convertWeights = (data) => {
+    let currWeight = 0;
+    for (let i = 0; i < data.length; i++) {
+        currWeight += data[i][0];
+        data[i][0] = currWeight;
+    }
+    return data;
+};
+
+const lerp = (start, end, t) => { return start * (1 - t)  + end * t; };
+const getInfo = (data, index) => {
+    if (index < 0) { index = 0; }
+    let prev = 0;
+    for (let line of data) {
+        if (index <= line[0]) {
+            let lerpData = [];
+            for (let i = 0; i < line[1].length; i++) { lerpData.push(lerp(line[1][i], line[2][i], (index - prev) / (line[0] - prev))); }
+            return lerpData;
+        }
+        prev = line[0];
+    }
+};
+
+// weight, from, to
+const cameraPositions = convertWeights([
+    [10, [10, 10, 10], [10, 10, 10]],
+    [20, [10, 10, 10], [3, 5, 10]],
+    [20, [3, 5, 10], [3, 2.5, 5]],
+    [20, [3, 2.5, 5], [3, 2.5, -3]],
+    [20, [3, 2.5, -3], [1, 5, -3]],
+]);
+
+const cameraRotations = convertWeights([
+    [10, [0, 30, 30], [0, 0, 0]],
+    // [10, [0, 0, 0], [0, 0, 0]],
+]);
 
 $(window).scroll((event) => {
     if ($(window).scrollTop() >= $(window).height() + $('.section').height() + 100) { $('.scrolling-kitchen.canvas-wrapper').css({ 'position': 'fixed' }); }
     else { $('.scrolling-kitchen.canvas-wrapper').css({ 'position': 'static' }); }
-    let scrollArea = $('body').height() - $(window).height() - $('.section').height() - 100;
-    let cameraIndex = $(window).scrollTop() - $(window).height() * 2 - $('.section').height() - 100;
-    if (cameraIndex / scrollArea > 0.5) {
-        // alert(cameraIndex / scrollArea);
-    }
+    
+    let scrollArea = $('body').height() - $(window).height() * 2 - $('.section').height() - 100;
+    let decimalScroll = $(window).scrollTop() - $(window).height() - $('.section').height() - 100;
+    
+    let positionInfo = getInfo(cameraPositions, (decimalScroll / scrollArea) * cameraPositions[cameraPositions.length - 1][0]);
+    camera.position.set(positionInfo[0], positionInfo[1], positionInfo[2]);
+    let rotationInfo = getInfo(cameraRotations, (decimalScroll / scrollArea) * cameraRotations[cameraRotations.length - 1][0]);
+    console.log(rotationInfo);
+    camera.rotation.set(rotationInfo[0] * Math.PI / 180, rotationInfo[1] * Math.PI / 180, rotationInfo[2] * Math.PI / 180);
 });
 
 renderer.setAnimationLoop(() => {
-    controls.update();
+    // controls.update();
     renderer.render(scene, camera);
     TWEEN.update();
-    // console.log(camera.position);
+    // console.log(camera.rotation);
 });
